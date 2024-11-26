8f815892eba0014fbb680b9e04cd6bd958b2b35b
Kiyan Mair <kiyan.mair@gmail.com>
2024-11-25 23:57:51 -0500

    refactor: Create Syncer to manage sync logic (#11)

--- cmd/shiftsync/main.go
+++ cmd/shiftsync/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 
-	"github.com/kiyanmair/shift-sync/config"
-	"github.com/kiyanmair/shift-sync/internal/destination"
-	"github.com/kiyanmair/shift-sync/internal/source"
 	"github.com/kiyanmair/shift-sync/internal/sync"
 )
 
@@ -22,49 +17,8 @@ func main() {
 		Use:   "run",
 		Short: "Execute syncs from sources to destinations",
 		Run: func(cmd *cobra.Command, args []string) {
-			cfg, err := config.LoadConfig(configPath)
-			if err != nil {
-				log.Fatalf("Failed to load config: %v", err)
-			}
-
-			sources := make(map[string]source.Source)
-			for _, srcCfg := range cfg.Sources {
-				src, err := source.NewSource(srcCfg)
-				if err != nil {
-					log.Printf("Failed to create source %s: %v", srcCfg.ID, err)
-					continue
-				}
-				sources[srcCfg.ID] = src
-			}
-
-			destinations := make(map[string]destination.Destination)
-			for _, destCfg := range cfg.Destinations {
-				dest, err := destination.NewDestination(destCfg)
-				if err != nil {
-					log.Printf("Failed to create destination %s: %v", destCfg.ID, err)
-					continue
-				}
-				destinations[destCfg.ID] = dest
-			}
-
-			var errs []error
-			for _, syncCfg := range cfg.Syncs {
-				err := sync.Sync(syncCfg, sources, destinations)
-				if err != nil {
-					errs = append(errs, err)
-					continue
-				}
-				log.Printf("Synced source %s to destination %s", syncCfg.SourceID, syncCfg.DestinationID)
-			}
-
-			numTotal := len(cfg.Syncs)
-			numFailed := len(errs)
-			numSuccessful := numTotal - numFailed
-
-			log.Printf("Completed %d/%d syncs", numSuccessful, numTotal)
-			if numFailed > 0 {
-				log.Fatalf("Encountered errors:\n%v", errors.Join(errs...))
-			}
+			syncer := sync.NewSyncer(configPath)
+			syncer.RunSyncs()
 		},
 	}
 
---
+++ internal/sync/syncer.go
@@ -0,0 +1,50 @@
+package sync
+
+import (
+	"log"
+
+	"github.com/kiyanmair/shift-sync/config"
+	"github.com/kiyanmair/shift-sync/internal/destination"
+	"github.com/kiyanmair/shift-sync/internal/source"
+)
+
+type Syncer struct {
+	cfg          *config.Config
+	sources      map[string]source.Source
+	destinations map[string]destination.Destination
+}
+
+func NewSyncer(configPath string) *Syncer {
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	sources := make(map[string]source.Source)
+	for _, srcCfg := range cfg.Sources {
+		src, err := source.NewSource(srcCfg)
+		if err != nil {
+			log.Printf("Failed to create source %s: %v", srcCfg.ID, err)
+			continue
+		}
+		sources[srcCfg.ID] = src
+	}
+
+	destinations := make(map[string]destination.Destination)
+	for _, destCfg := range cfg.Destinations {
+		dest, err := destination.NewDestination(destCfg)
+		if err != nil {
+			log.Printf("Failed to create destination %s: %v", destCfg.ID, err)
+			continue
+		}
+		destinations[destCfg.ID] = dest
+	}
+
+	syncer := Syncer{
+		cfg:          cfg,
+		sources:      sources,
+		destinations: destinations,
+	}
+
+	return &syncer
+}
