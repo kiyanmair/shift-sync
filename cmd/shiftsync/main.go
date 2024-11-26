e1fa5c046c1cae4855b98cb3d0efafdf8ae9318e
Kiyan Mair <kiyan.mair@gmail.com>
2024-11-26 01:07:02 -0500

    style: Rename integration dir to integrations (plural)

--- cmd/shiftsync/main.go
+++ cmd/shiftsync/main.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/spf13/cobra"
 
-	_ "github.com/kiyanmair/shift-sync/internal/integration/destination"
-	_ "github.com/kiyanmair/shift-sync/internal/integration/source"
+	_ "github.com/kiyanmair/shift-sync/internal/integrations/destination"
+	_ "github.com/kiyanmair/shift-sync/internal/integrations/source"
 	"github.com/kiyanmair/shift-sync/internal/sync"
 )
 
