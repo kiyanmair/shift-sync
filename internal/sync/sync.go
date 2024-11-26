c4a120c04581184dfed31eeeaa529fd455c24678
Kiyan Mair <kiyan.mair@gmail.com>
2024-11-25 23:53:52 -0500

    refactor: Create Syncer to manage sync logic

--- internal/sync/sync.go
+++ internal/sync/sync.go
@@ -1,21 +1,41 @@
 package sync
 
 import (
+	"errors"
 	"fmt"
+	"log"
 
 	"github.com/kiyanmair/shift-sync/config"
-	"github.com/kiyanmair/shift-sync/internal/destination"
-	"github.com/kiyanmair/shift-sync/internal/source"
 )
 
-func Sync(syncCfg config.Sync, sources map[string]source.Source, destinations map[string]destination.Destination) error {
-	source, sourceExists := sources[syncCfg.SourceID]
-	if !sourceExists {
+func (s *Syncer) RunSyncs() {
+	var errs []error
+	for _, syncCfg := range s.cfg.Syncs {
+		if err := s.singleSync(syncCfg); err != nil {
+			errs = append(errs, err)
+		} else {
+			log.Printf("Synced source %s to destination %s", syncCfg.SourceID, syncCfg.DestinationID)
+		}
+	}
+
+	numTotal := len(s.cfg.Syncs)
+	numFailed := len(errs)
+	numSuccessful := numTotal - numFailed
+
+	log.Printf("Completed %d/%d syncs", numSuccessful, numTotal)
+	if numFailed > 0 {
+		log.Fatalf("Encountered errors:\n%v", errors.Join(errs...))
+	}
+}
+
+func (s *Syncer) singleSync(syncCfg config.Sync) error {
+	source, exists := s.sources[syncCfg.SourceID]
+	if !exists {
 		return fmt.Errorf("source %s not found", syncCfg.SourceID)
 	}
 
-	dest, destExists := destinations[syncCfg.DestinationID]
-	if !destExists {
+	dest, exists := s.destinations[syncCfg.DestinationID]
+	if !exists {
 		return fmt.Errorf("destination %s not found", syncCfg.DestinationID)
 	}
 
