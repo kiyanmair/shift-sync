750838580e72617c1364e3c802876cc24e6bbc48
Kiyan Mair <kiyan.mair@gmail.com>
2024-11-28 01:37:16 -0500

    refactor: Create types for constructor funcs

--- internal/core/registry.go
+++ internal/core/registry.go
@@ -2,9 +2,11 @@ package core
 
 import "github.com/kiyanmair/shift-sync/config"
 
-var sourceRegistry = map[string]func(config.Source) (Source, error){}
+type sourceConstructor func(config.Source) (Source, error)
+type destinationConstructor func(config.Destination) (Destination, error)
 
-var destinationRegistry = map[string]func(config.Destination) (Destination, error){}
+var sourceRegistry = map[string]sourceConstructor{}
+var destinationRegistry = map[string]destinationConstructor{}
 
 func RegisterSource(name string, constructor func(config.Source) (Source, error)) {
 	sourceRegistry[name] = constructor
